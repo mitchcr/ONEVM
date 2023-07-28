1b47b27d17379743f1166eeedc899b7010b69472
mitchcr <44411691+mitchcr@users.noreply.github.com>
2023-07-28 11:40:44 -0600

    Create Lab3Initrd_Script.sh

---
+++ Initrd/Lab3Initrd_Script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Custom script to damage Lab2 for Initrd module.
+
+#Commenting out add_drivers+="hv_vmbushv_netvschv_storvsc" in /etc/dracut.conf
+sed -i 's/add_drivers+="hv_vmbus hv_netvsc hv_storvsc"/\#add_drivers+="hv_vmbus hv_netvsc hv_storvsc"/' /etc/dracut.conf
+
+#Get the hv_storvsc module not becoming part of the initrd file.
+echo "omit_drivers+=\"hv_vmbus hv_netvsc hv_storvsc\"" >> /etc/dracut.conf
+
+#Recreate the initrd
+dracut -f /boot/initramfs-`uname -r`.img `uname -r`
+
+#Reboot the VM
+reboot
