e8ade0a8b5711937abe340d4fd428d12a90b35a7
mitchcr <44411691+mitchcr@users.noreply.github.com>
2023-09-20 14:54:51 -0600

    Add files via upload

---
+++ ServiceTroubleshooting/service_lab1_script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+wget "https://linuxspecialist.blob.core.windows.net/bin/breaker?sp=r&st=2021-08-06T13:34:17Z&se=2030-08-06T21:34:17Z&spr=https&sv=2020-08-04&sr=b&sig=d9CQTzEpqW5pewdgms0Zi2cpxKJx9AT0DE7wbzDngFY%3D" -O breaker
+chmod +x ./breaker
+./breaker
