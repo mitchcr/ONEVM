cdf4e14378624d0857174ce8e4c5f76e08a331b9
mitchcr <44411691+mitchcr@users.noreply.github.com>
2024-01-08 11:23:52 -0600

    Delete RepositoryIssues/RepositoryIssues-Lab4-breakscript.sh

--- RepositoryIssues/RepositoryIssues-Lab4-breakscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Custom script to damage Lab2 for Initrd module
-
-cd /etc/products.d
-unlink baseproduct
-ln -s sle-module-toolchain.prod baseproduct 
-registercloudguest --force-new
-sleep 10
